a3e7705339cc8c98cb09f59df4614e5e64e78f2c
wingedmachine <wheeler.pryor@gmail.com>
2018-12-17 15:52:21 -0500

    Done.

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -33,14 +33,6 @@ RSpec.describe Song, type: :model do
     end
   end
 
-  describe 'note_contents' do
-    it 'returns the content of all notes as an array' do
-      song = Song.new
-      song.note_contents = ['hi', 'there']
-      expect(song.note_contents).to eq ['hi', 'there']
-    end
-  end
-
   describe 'note_contents=' do
     it 'sets notes for a song' do
       song = Song.new
@@ -64,6 +56,14 @@ RSpec.describe Song, type: :model do
       song = Song.new
       song.note_contents = ['', "i'm all alone", '', '']
       expect(song.note_contents).to eq ["i'm all alone"]
+    end
+  end
+
+  describe 'note_contents' do
+    it 'returns the content of all notes as an array' do
+      song = Song.new
+      song.note_contents = ['hi', 'there']
+      expect(song.note_contents).to eq ['hi', 'there']
     end
   end
 
