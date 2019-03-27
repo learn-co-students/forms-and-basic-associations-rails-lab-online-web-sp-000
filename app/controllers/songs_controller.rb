8090bad953d8a5ea64724f8e7d86f6c7a3ac9a83
Jcg408 <jcg321@comcas.net>
2019-03-27 13:08:34 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,6 +13,7 @@ class SongsController < ApplicationController
   end
   
   def create
+    # binding.pry
     @song = Song.new(song_params)
     if @song.save
       redirect_to @song
@@ -46,7 +47,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name, :genre_name, :note_contents =>[])
+    params.require(:song).permit(:title, :artist_name, :genre_id, :note_contents =>[])
   end
 end
 
