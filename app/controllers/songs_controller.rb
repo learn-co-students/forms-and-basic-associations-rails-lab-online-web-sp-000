2762390eeb3173f7c65453085ed3dd5dac40fcfc
github username <github email address>
2019-04-01 13:47:24 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,6 +47,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:artist_name, :title)
+    params.require(:song).permit(:artist_name, :genre_id, :title)
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -14,4 +14,12 @@ class Song < ActiveRecord::Base
       self.artist ? self.artist.name : nil
    end
 
+   def genre_name=(name)
+      self.genre = Genre.find_or_create_by(name: name)
+    end
+
+    def genre_name
+       self.artist ? self.genre.name : nil
+    end
+
 end
