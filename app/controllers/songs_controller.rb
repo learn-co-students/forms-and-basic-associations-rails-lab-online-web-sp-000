fe6c998efcdd20f58d4d5f99c326d7c8104a194a
Btate712 <btate712@gmail.com>
2019-05-13 07:43:52 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,6 +4,7 @@ class SongsController < ApplicationController
   end
 
   def show
+    @params = params
     @song = Song.find(params[:id])
   end
 
@@ -47,7 +48,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_name, :note_contents => [])
   end
 end
-
