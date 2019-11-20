1f2e7dcab27c9b0c2e7aea6e710d2aae36c33b83
Eric Clark <eclark0013@gmail.com>
2019-11-20 11:13:49 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,34 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
+  def notes=(input)
+    if input.is_a? Array
+      input.each do |content|
+        note = Note.new(content: content)
+        self.notes << note
+      end
+    else
+      self.notes << input
+    end
+  end
+  
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the Rails application.
+require 'pry-rails'
 require_relative 'application'
-
 # Initialize the Rails application.
 Rails.application.initialize!
