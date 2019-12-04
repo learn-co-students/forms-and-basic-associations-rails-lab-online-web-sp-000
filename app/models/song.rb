e428678d1daeaae2b54bef40caf04b2fc548797f
Bernadette Davis <bsd231@nyu.edu>
2019-12-04 02:39:37 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,21 @@
 class Song < ActiveRecord::Base
+  has_many :notes
+  belongs_to :artist
+  belongs_to :genre
   # add associations here
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def song_artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def song_artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+
+
 end
