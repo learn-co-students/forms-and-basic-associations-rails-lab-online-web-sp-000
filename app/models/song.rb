146d6702cd8cabe900aeb0d2bc0fbb6a440b131a
nicholas-wilson <nicholas.wilson@nevada.unr.edu>
2020-03-11 20:36:09 -0700

    figuring out how to add notes

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,13 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :genre
+  belongs_to :artist
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
 end
