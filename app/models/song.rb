32e3442a6da8a7bb7f9950e6f82688e329bd096f
Josh Ellis <joshe523@gmail.com>
2020-04-02 09:35:06 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,23 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name
+    self.try(:artist).try(:name)
+  end
+
+  def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
+  end
+
+  def note_contents=(notes)
+    notes.each do |content|
+      if content.strip.size > 0
+          self.notes.build(content: content)
+      end
+    end
+  end
+
 end
