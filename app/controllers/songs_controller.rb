3fda2b46ab0ece2ec54b7345082e7c1eb3347490
Will Carter <will.carter@fergusllc.com>
2020-04-03 16:09:43 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,11 +9,14 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    @song.notes << Note.new
+    @song.notes << Note.new
   end
 
   def create
-    @song = Song.new(song_params)
 
+    @song = Song.new(song_params)
+    
     if @song.save
       redirect_to @song
     else
@@ -47,7 +50,8 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, notes_attributes: [:content])
   end
+
 end
 
