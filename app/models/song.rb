a69cb49dbca31c92352365e9083b8c8c158159fa
gabejoseph <gabriel.joseph4@gmail.com>
2020-06-30 11:44:59 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -15,4 +15,19 @@ class Song < ActiveRecord::Base
     self.artist ? self.artist.name : nil
   end 
 
+
+  def note_contents=(content)
+    content.each do |content|
+      self.notes << Note.find_or_create_by(content: content)
+    end
+  end
+
+  def note_contents
+    ret = []
+    self.notes.each do |note|
+      ret << note.content unless note.content.empty?
+    end
+    ret
+  end
+
 end
