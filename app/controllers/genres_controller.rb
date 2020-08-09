d450223c3ffbfc212a489f9c584615ef80d08fb4
cantstopcoding <jonathan_koss@yahoo.com>
2020-08-09 11:41:53 -0400

    Done.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,36 @@
+class ArtistsController < ApplicationController
+    def index
+
+    end
+  
+    def show
+
+    end
+  
+    def new
+
+    end
+  
+    def create
+      
+    end
+  
+    def edit
+
+    end
+  
+    def update
+      
+    end
+  
+    def destroy
+      
+    end
+  
+    private
+  
+    def genre_params
+      params.require(:genre).permit!
+    end
+  end
+  
