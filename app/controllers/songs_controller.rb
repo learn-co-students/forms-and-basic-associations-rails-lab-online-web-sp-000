033186b878e6ea35c211b3fe8a003c82d39a6920
chienleow <chienleow1@gmail.com>
2020-08-10 21:12:37 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,7 +12,7 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.new(song_params)
+    @song = Song.new(song_params(:title, :artist_name, :genre_id, :notes_id))
 
     if @song.save
       redirect_to @song
@@ -28,7 +28,7 @@ class SongsController < ApplicationController
   def update
     @song = Song.find(params[:id])
 
-    @song.update(song_params)
+    @song.update(song_params(:title, :artist, :genre, :notes_id))
 
     if @song.save
       redirect_to @song
@@ -46,8 +46,8 @@ class SongsController < ApplicationController
 
   private
 
-  def song_params
-    params.require(:song).permit(:title)
+  def song_params(*args)
+    params.require(:song).permit(*args)
   end
 end
 
