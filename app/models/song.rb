984797c025cbcee70cb86b9fab53acc3e9b0936b
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-09-14 06:34:29 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -10,4 +10,24 @@ class Song < ActiveRecord::Base
   def artist_name
     self.artist ? self.artist.name : nil
   end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
+  def song_notes=(contents)
+    contents.each do |content|
+      if content.strip != ""
+        self.notes.build(content: content)
+      end 
+    end 
+  end
+
+  def song_notes
+    self.notes.map(&:content)
+  end
 end
