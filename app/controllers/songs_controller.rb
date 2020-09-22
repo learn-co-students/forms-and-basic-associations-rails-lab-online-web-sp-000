9db7ecec8e56ae551b441bd1944c111dc1b19fe9
AustinRhoads <austinrrhoads@ymail.com>
2020-09-21 20:46:02 -0500

    I thought id have to follow him forever.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,6 +5,7 @@ class SongsController < ApplicationController
 
   def show
     @song = Song.find(params[:id])
+    
   end
 
   def new
@@ -12,8 +13,9 @@ class SongsController < ApplicationController
   end
 
   def create
+    
     @song = Song.new(song_params)
-
+    
     if @song.save
       redirect_to @song
     else
@@ -47,7 +49,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, :note_contents => [])
   end
 end
 
