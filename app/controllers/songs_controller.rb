4e420069f644ce202c2c9f9183085bc62631844a
ismayton <ian.mayton@gmail.com>
2020-09-24 12:49:35 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,6 +13,9 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.new(song_params)
+    if !params[:song][:notes][0].empty? || !params[:song][:notes][1].empty?
+      @song.notes = params[:song][:notes]
+    end 
 
     if @song.save
       redirect_to @song
@@ -47,7 +50,8 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, note_ids: [])
   end
+
 end
 
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,30 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end 
+  
+  def artist_name 
+    self.artist ? self.artist.name : nil
+  end 
+
+  def genre_id=(name)
+
+    self.genre = Genre.find_or_create_by(name: name)
+  end 
+  
+  def genre_id 
+    self.genre ? self.genre.id : nil
+  end 
+
+  def notes=(note_array)
+    note_array.each do |content|
+      note = Note.create(content: content)
+      self.notes << note
+    end
+  end
+
 end
