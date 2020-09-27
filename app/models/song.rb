7fcf8bfe0743cbcb1710a498e107b2eb76e9cde5
ArslanInfinite <ali.arslan319@gmail.com>
2020-09-27 19:21:08 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -12,12 +12,11 @@ class Song < ActiveRecord::Base
     self.artist ? self.artist.name : nil
   end
 
-  def genre_name=(name)
-    self.genre = Genre.find_or_create_by(name: name)
-  end
-
-  def genre_name
-    self.genre ? self.genre.name : nil
-  end
+  def note_ids=(ids)
+     ids.each do |id|
+       note = Note.find(id)
+       self.notes << note
+     end
+   end
 
 end 
