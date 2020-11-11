96b3a9562155c34898a1261c6cd0358095158c09
Chad Lengefeld <chadlengefeld@gmail.com>
2020-11-11 06:18:29 +0000

    Done.

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,4 @@
 class Note < ActiveRecord::Base
   # add associations here
-  content :String
   belongs_to :song
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -20,5 +20,17 @@ class Song < ActiveRecord::Base
     self.genre ? self.genre.name : nil
   end
 
+  def note_contents=(contents)
+   contents.each do |content|
+     if content.strip != ""
+        self.notes.build(content: content)
+     end
+   end
+ end
+
+ def note_contents
+   self.notes.map(&:content)
+ end
+
 
 end
