93ba9402713bcef0a7e5489332943cce8d07cd99
Ryan Burns <ryankburns@gmail.com>
2021-01-27 16:02:04 -0800

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -28,6 +28,7 @@ class SongsController < ApplicationController
   def update
     @song = Song.find(params[:id])
 
+
     @song.update(song_params)
 
     if @song.save
@@ -47,7 +48,8 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, note_contents: [])
   end
+
 end
 
