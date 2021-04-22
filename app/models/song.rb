af0d91c442ea6a8ab370e34654ca42a7c02a87f1
Allison <allimacq@gmail.com>
2021-04-21 23:13:46 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,38 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  has_many :notes
+  belongs_to :genre
+  belongs_to :artist
+
+  #accepts_nested_attributes_for :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+    def notes_1=(content)
+      note_1 = Note.find_or_create_by(id: 1)
+      note_1.content = content
+      self.notes << note_1
+    end
+
+    def notes_2=(content)
+      note_2 = Note.find_or_create_by(id: 2)
+      note_2.content = content
+      self.notes << note_2
+    end
+
+    def notes_1
+      self.notes.first.content
+    end
+
+    def notes_2
+      self.notes.second ? self.notes.second.content : nil
+
+    end
+
+
 end
--- db/migrate/20160114013240_add_song_id_to_notes.rb
+++ db/migrate/20160114013240_add_song_id_to_notes.rb
@@ -1,5 +1,6 @@
 class AddSongIdToNotes < ActiveRecord::Migration
   def change
     add_column :notes, :song_id, :integer
+    add_column :notes, :notes_1, :string
   end
 end
